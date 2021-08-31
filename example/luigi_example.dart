20c5ff6e66bbcd83153e10a34950828240eaddc8
Ravi Teja Gudapati <tejainece@gmail.com>
2021-08-31 17:43:00 +0200

    luigi example in big endian

--- example/luigi_example.dart
+++ example/luigi_example.dart
@@ -1,15 +1,8 @@
 import 'dart:typed_data';
 
 import 'package:ninja/ninja.dart';
-import 'package:ninja_ed25519/ninja_ed25519.dart' hide PrivateKey, PublicKey;
 import 'package:ninja_ed25519/curve.dart';
 import 'package:monero_jsonrpc/monero_jsonrpc.dart';
-
-BigInt scalar(String hex) {
-  return BigInt.parse(hex, radix: 16)
-      .asBytes(outLen: 32, endian: Endian.little)
-      .asBigInt();
-}
 
 void step1() {
   final AStr =
@@ -18,10 +11,9 @@ void step1() {
 
   final rStr =
       'c91ae3053f640fcad393fb6c74ad9f064c25314c8993c5545306154e070b1f0f';
-  final r = scalar(rStr);
-  final D = A.multiplyScalar(r);
-  print(D.asCompressedHex);
-  print(D.multiplyScalar(BigInt.from(8)).asCompressedHex);
+  final r = Scalar.fromString(rStr, radix: 16, endian: Endian.big).value;
+  final D = A.multiplyScalar(r).multiplyScalar(BigInt.from(8));
+  print(D.asCompressedHex(endian: Endian.big));
   // a1d198629fadc698b48f33dc2e280301679ab2c75a76974fd185ba66ab8418cc
   print('----------');
 }
@@ -33,9 +25,10 @@ void step2() {
 
   final aStr =
       'fadf3558b700b88936113be1e5342245bd68a6b1deeb496000c4148ad4b61f02';
-  final a = scalar(aStr);
-  final D = R.multiplyScalar(a);
-  print(D.asCompressedHex);
+  final a = Scalar.fromString(aStr, radix: 16, endian: Endian.big).value;
+  // final a = scalar(aStr);
+  final D = R.multiplyScalar(a).multiplyScalar(BigInt.from(8));
+  print(D.asCompressedHex(endian: Endian.big));
   // a1d198629fadc698b48f33dc2e280301679ab2c75a76974fd185ba66ab8418cc
   print('----------');
 }
@@ -53,28 +46,15 @@ void address() {
 void computeR() {
   final rStr =
       'c91ae3053f640fcad393fb6c74ad9f064c25314c8993c5545306154e070b1f0f';
-  final r = scalar(rStr);
-  final R = Curve25519.G.multiplyScalar(r);
-  print(R.asCompressedHex);
-  print('----------');
-}
-
-void computeR2() {
-  final rStr =
-      'c91ae3053f640fcad393fb6c74ad9f064c25314c8993c5545306154e070b1f0f';
-  final r = scalar(rStr);
-  final GStr =
-      '5866666666666666666666666666666666666666666666666666666666666666';
-  final G = Point25519.fromHex(GStr);
-  final R = G.multiplyScalar(r);
-  print(R.asCompressedHex);
+  final r = Scalar.fromString(rStr, radix: 16, endian: Endian.big).value;
+  final R = curve25519.scalarMultiplyBase(r);
+  print(R.asCompressedHex(endian: Endian.big));
   print('----------');
 }
 
 void main() {
   address();
   computeR();
-  computeR2();
   step1();
   step2();
   // TODO
